3e2b9173982697e2ee68c0bf6c51f2708c44fbe5
Diego Rodrigues <diego.rodrigues@rgc.com.br>
2022-10-24 14:38:47 -0300

    Listagem das contas utilizando GET

--- main.py
+++ main.py
@@ -1,13 +1,39 @@
-from datetime import datetime
+from pydantic import BaseModel, Field
+import uvicorn
 from typing import Dict
 from fastapi import FastAPI
+from datetime import datetime
 
 app = FastAPI()
 
+accounts = []
+
+class Account(BaseModel):
+    id: int
+    nome: str
+    documento: str
+    dataNacimento: str
+    email: str
+    telefone: str
+
 @app.get("/")
 def root() -> str:
-    return "New Life Bank"
+    return "Jujubinha"
 
-@app.get("/health/")
+@app.get("/health")
 def alive() -> Dict[str, datetime]:
     return {"timestamp": datetime.now()}
+
+@app.post("/accounts", response_model=bool, tags=["accounts"])
+def create_account(account: Account):
+
+    accounts.append(account)
+    
+    return True
+
+@app.get("/accounts")
+def list_account () -> accounts:
+    return {"lista": accounts}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8001, reload=True)
